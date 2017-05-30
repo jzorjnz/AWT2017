18f2a9e8fbd899d9b46de08828a8a6d08a166637
Syed Zeeshan Akhtar <akhtar.syedzeeshan@gmail.com>
2017-05-30 06:00:14 +0200

    Testing templates

--- pages/startup/viewmodel.js
+++ pages/startup/viewmodel.js
@@ -29,13 +29,14 @@ function viewModelMain() {
         // You can add custom post-processing logic here
         console.log('Running logic for view model main...');
         $('#startup-template').load('pages/startup/view.html', function() {
-        //knockout binding goes here
+            //knockout binding goes here
             self.curTemplate('startup-template')
             alert('Load was performed.');
             var element = $('#main_view')[0]; 
             ko.cleanNode(element);
+            ko.applyBindings(new viewModelStartup(), document.getElementById('startup_view'));
             console.log('applying bindings...')
-    });
+        });
 
         
     }
@@ -140,7 +141,6 @@ function viewModel5() {
 
 // Activate knockout.js
 ko.applyBindings(new viewModelMain(), document.getElementById('main_view'));
-ko.applyBindings(new viewModelStartup(), document.getElementById('startup_view'));
             
 /*
 ko.applyBindings(new viewModel2(), document.getElementById('startup_view_2'));
